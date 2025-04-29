e956adb3485d687b8ac4908e3bbbb767274df363
Heidy <heidy.subs@gmail.com>
2025-04-28 21:57:50 -0500

    modal

--- Sidebar-options/inventario_script.js
+++ Sidebar-options/inventario_script.js
@@ -67,7 +67,15 @@ document.addEventListener("DOMContentLoaded", function () {
     updateTable();
 });
 
-//DATA TABLE
+//MODALS
+const open = document.getElementById("open");
+const modal_container = document.getElementById("modal_container");
+const close = document.getElementById("close");
 
+open.addEventListener("click", () => {
+    modal_container.classList.add("show");
+});
 
-
+close.addEventListener("click", () => {
+    modal_container.classList.remove("show");
+});
